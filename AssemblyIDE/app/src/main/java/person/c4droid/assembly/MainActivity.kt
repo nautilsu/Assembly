43b496a5cb39a59cde01349d8d7b3d1b35b72215
c4droid <c4droid@foxmail.com>
2017-10-06 20:09:36 +0800

    Build Base Startup Dialog

---
+++ AssemblyIDE/app/src/main/java/person/c4droid/assembly/MainActivity.kt
@@ -0,0 +1,33 @@
+package person.c4droid.assembly
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*
+        * at program start,
+        * if not editing file,
+        * show create project dialog,
+        * else do nothing.
+        */
+        val item = arrayOf("GNU as","nasm")
+        val dialog = AlertDialog.Builder(this@MainActivity)
+        dialog.setTitle("New Project...")
+        dialog.setItems(item) {
+            dialog,which ->
+            // if click the GNU as,call gnuDialog,else call nasmDialog
+            when (item[which].toString()) {
+                "GNU as" -> {
+                    // gnuDialog()
+                    // TODO: create GNU as Project
+                }
+                "nasm" -> {
+                    // nasmDialog()
+                    // TODO: create nasm project
+                }
+            }
+        }
+        dialog.show()
+    }
+}
